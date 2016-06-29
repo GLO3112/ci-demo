f809eb388938e42c85a96e7f28ed30bc053dc231
William Fortin <wfortin@coveo.com>
2016-06-29 19:55:11 -0400

    npm start

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.use('/', express.static(__dirname + '/'));
+app.set('port', (process.env.PORT || 8080));
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});

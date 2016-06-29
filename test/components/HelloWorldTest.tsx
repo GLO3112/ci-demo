08e6e687472e4e18730162cb030800d006bae0ab
William Fortin <wfortin@coveo.com>
2016-06-28 21:44:26 -0400

    Initial commit

---
+++ test/components/HelloWorldTest.tsx
@@ -0,0 +1,20 @@
+import test from 'ava';
+import * as React from 'react';
+import {shallow} from 'enzyme';
+import {HelloWorld, HelloWorldProps} from '../../src/components/HelloWorld';
+
+test('HelloWorld should be in a h1 title', t => {
+    const wrapper = shallow(<HelloWorld
+        firstname="GLO-3112"
+        lastname="WEB"/>);
+
+    t.is(wrapper.find('h1').length, 1);
+});
+
+test('HelloWorld props are defined properly', t => {
+    const wrapper = shallow(<HelloWorld
+        firstname="GLO-3112"
+        lastname="WEB"/>);
+
+    t.is(wrapper.find('h1').text(), 'Hello GLO-3112 WEB!');
+});

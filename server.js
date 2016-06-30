01344f65bf5e9a9179fb8f670210904a94fc31c9
William Fortin <wfortin@coveo.com>
2016-06-29 21:06:31 -0400

    Allow dist tu be pushed for heroku... I'll need a better fix

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use('/', express.static(__dirname + '/'));
+app.use('/', express.static(__dirname + '/files'));
 app.set('port', (process.env.PORT || 8080));
 
 app.listen(app.get('port'), function() {

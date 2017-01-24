eb9bbb5ece3e51df9081a636091773b9a80f511c
William Fortin <wfortin@Williams-MacBook-Pro.local>
2017-01-24 13:04:53 -0500

    Example for untyped definitions

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -6,6 +6,10 @@ export interface HelloWorldProps {
 }
 
 export class HelloWorld extends React.Component<HelloWorldProps, any> {
+    test() {
+      console.log(R.join(', ')(['Doe','Joe']));
+    }
+
     render() {
         return <h1>
             HELLO {this.props.firstname} {this.props.lastname}!
